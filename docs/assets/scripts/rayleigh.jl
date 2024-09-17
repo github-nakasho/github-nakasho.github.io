fcc67ebdd5b4c6b687785488e396e537b69635a1
Sho NAKAMURA <nakasho@Shos-iMac.local>
2024-09-16 18:06:42 -0600

    Upgrade jekyll

--- docs/assets/scripts/rayleigh.jl
+++
@@ -1,9 +0,0 @@
-using Plots
-gr()
-
-len = 200
-omega = range(0.0, 6.0, length=len)
-gamma = 0.5
-rayleigh = omega.^4 ./ ((omega.^2 .- 1).^2 .+ gamma * omega.^2)
-plot(omega, rayleigh, linewidth=3, xlabel=raw"\omega", ylabel=raw"\sigma (\omega) / \sigma_{T}", label=false)
-savefig("rayleigh.png")

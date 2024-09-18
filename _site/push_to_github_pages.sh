6382d9817b27c86f7778dbb189c849739f0389f9
Sho NAKAMURA <nakasho@Shos-iMac.local>
2024-09-18 00:20:05 -0600

    Modify .github/workflows/github-pages.yml

--- _site/push_to_github_pages.sh
+++ _site/push_to_github_pages.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-git config --global http.version HTTP/1.1
-git config --global http.postBuffer 524288000
-git config --global http.lowSpeedLimit 0
-git config --global http.lowSpeedTime 999999
 git push origin master
 git checkout --orphan gh-pages
 git checkout master
